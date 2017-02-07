b9c33e3e1169e5c20d7b4288b563bc4da0c7eb0c
Ellen Chang <imnunu@gmail.com>
2017-02-07 18:20:38 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,33 @@
+//var raining = true;
+//var cold = false;
+
+//if (raining){
+
+//	console.log("Don't forget your umbrella!");
+//}
+var temperature = 15;
+
+if (temperature < 0){
+console.log("Make sure you pick out a scarf!");
+
+} else if(temperature < 15){
+
+	console.log("Short sleeves won't cut it!");
+
+} else{
+	console.log("Short sleeves are fine.");
+
+}
+
+console.log("Now you're ready to go outside!");
+
+//if (cold){
+
+//	console.log("Make sure you pick out a scarf!");
+//} else{
+
+//	console.log("Short sleeves are fine.");
+//}
+
+//console.log("Now you're ready to go outside!");
+
