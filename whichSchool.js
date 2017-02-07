a043deabc3293d62dd883446aaa19d0f20a39c06
Ellen Chang <imnunu@gmail.com>
2017-02-07 19:37:47 +0000

    Adds looping exercises

---
+++ whichSchool.js
@@ -0,0 +1,21 @@
+function whichSchool(age) {
+  // Your code in here ...
+  if ( age < 13 ){
+    return ("Elementary School");
+  }
+  else if ( age > 18 ){
+    return ("Lighthouse Labs");
+  }
+  else {
+    return ("Secondary School");
+  }
+}
+
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
+
